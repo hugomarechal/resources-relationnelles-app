6c200f1ac05b93bbe13d0248c1e76ba99c9e6f3b
ValHDL <valerie.houdaille@hinodev.com>
2025-04-17 16:32:17 +0200

    creation input

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1 className='text-4xl text-[#000000]'>Test title</h1>
+      <h1 className="text-4xl text-[#000000]">Test title</h1>
 
-      <button className='flex bg-red-500'>
-        test button
-      </button>
+      <button className="flex bg-red-500">test button</button>
+
+      <br />
+
+      {/* <FloatingInput
+        type={"text"}
+        label={"Nom"}
+        value={""}
+        name={""}
+        placeholder={"Entrez votre nom"}
+        error={false}
+      ></FloatingInput> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
