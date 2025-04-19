a56227c39d4d5c3ae99c0f7653386d82b9fd62fd
ValHDL <valerie.houdaille@hinodev.com>
2025-04-19 09:31:18 +0200

    select component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import { useState } from "react";
 import "./App.css";
-import SelectBox  from "./Components/Form/SelectBox";
+import Button from "./Components/Divers/Button";
 
 function App() {
+  const handleClick = () => {
+    alert("Bouton cliqué !");
+  };
 
-  const options = [
-    { value: '1', label: 'Chocolate' },
-    { value: '2', label: 'Strawberry' },
-    { value: '3', label: 'Vanilla' },
-  ];
+  // const options = [
+  //   { value: "1", label: "Chocolate" },
+  //   { value: "2", label: "Strawberry" },
+  //   { value: "3", label: "Vanilla" },
+  // ];
 
-  const [selectedOption, setSelectedOption] = useState(null);
+  // const [selectedOption, setSelectedOption] = useState(null);
 
   return (
     <>
       <h1 className="text-4xl text-[#000000]">Test title</h1>
-      <SelectBox
+      <br />
+      <Button label={"Bouton test"} onClick={handleClick}></Button>
+
+      {/* <SelectBox
         value={selectedOption}
         options={options}
         onChange={setSelectedOption}
         isDisabled={false}
       />
-      {selectedOption && (
-        <p>Tu as choisi : {selectedOption.label}</p>
-      )}
-      <button className="flex bg-red-500">test button</button>
+      {selectedOption && <p>Tu as choisi : {selectedOption.label}</p>}
+      <button className="flex bg-red-500">test button</button> */}
     </>
   );
 }
