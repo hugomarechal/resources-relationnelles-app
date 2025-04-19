e066ff9a89c82aa4b15a572c7a0cf93518a1fe14
ValHDL <valerie.houdaille@hinodev.com>
2025-04-19 11:29:37 +0200

    Select et floatinglabel components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,69 @@
+import { ChangeEvent, useState } from "react";
 import "./App.css";
 import Button from "./Components/Divers/Button";
+import FloatingInput from "./Components/Form/FloatingInput";
+import { SelectBox, SelectOption } from "./Components/Form/SelectBox";
 
 function App() {
+  // Floating input
+  const [inputValue, setInputValue] = useState("");
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  // Liste déroulante
+  const musicStyles = [
+    { value: "1", label: "Rock" },
+    { value: "2", label: "Jazz" },
+    { value: "3", label: "Blues" },
+  ];
+  const options: SelectOption[] = [
+    { label: "Selectionner une musique", value: "" },
+    ...musicStyles.map((musicStyle) => ({
+      label: musicStyle.label,
+      value: musicStyle.value,
+    })),
+  ];
+  const [value, setValue] = useState("");
+  const onChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    setValue(event.target.value);
+  };
+
+  // Bouton
   const handleClick = () => {
     alert("Bouton cliqué !");
   };
 
-  // const options = [
-  //   { value: "1", label: "Chocolate" },
-  //   { value: "2", label: "Strawberry" },
-  //   { value: "3", label: "Vanilla" },
-  // ];
-
-  // const [selectedOption, setSelectedOption] = useState(null);
-
   return (
     <>
-      <h1 className="text-4xl text-[#000000]">Test title</h1>
-      <br />
-      <Button label={"Bouton test"} onClick={handleClick}></Button>
+      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          (RE)SOURCES RELATIONNELLES
+        </span>
+      </h1>
 
-      {/* <SelectBox
-        value={selectedOption}
-        options={options}
-        onChange={setSelectedOption}
-        isDisabled={false}
-      />
-      {selectedOption && <p>Tu as choisi : {selectedOption.label}</p>}
-      <button className="flex bg-red-500">test button</button> */}
+      <br />
+
+      <form>
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
+          <div>
+            <FloatingInput
+              type={"email"}
+              label={"Nom"}
+              value={inputValue}
+              name={"nom"}
+              onChange={handleInputChange}
+            ></FloatingInput>
+          </div>
+          <div>
+            <SelectBox options={options} value={value} onChange={onChange} name={"music"} />
+            {value && (
+              <p className="text-xs">Id musique sélectionnée {value}</p>
+            )}
+          </div>
+        </div>
+        <Button label={"Bouton test"} onClick={handleClick}></Button>
+      </form>
     </>
   );
 }
--- src/Components/Form/SelectBox.tsx
+++ src/Components/Form/SelectBox.tsx
@@ -1,26 +1,52 @@
-import React from 'react'
-import Select from 'react-select'
+import React, { ChangeEvent } from "react";
 
 type SelectOption = {
-  value: string
-  label: string
+  label: string;
+  value: string;
+};
+
+interface Props {
+  options: SelectOption[];
+  value: string;
+  name: string;
+  disabled?: boolean;
+  required?: boolean;
+  helperText?: string;
+  onChange: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
 
-interface SelectBoxProps {
-  value: SelectOption | null;
-  options: SelectOption[]
-  isDisabled?: boolean
-  onChange: (option: SelectOption | null) => void;
-}
-
-const SelectBox: React.FC<SelectBoxProps> = ({
-  value,
-  options,
-  isDisabled = false,
-  onChange,
-}) => {
+const SelectBox = ({ options, value, name,
+  required,
+  disabled,
+  helperText, onChange }: Props) => {
   return (
-  <Select options={options} value={value} isDisabled={isDisabled} onChange={onChange}/>
-)}
+    <div className="relative z-0 w-full mb-5 group">
+      <select
+        name={name}
+        id={name}
+        className="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer"
+        disabled={disabled}
+        onChange={onChange}
+        required={required}
+        value={value}
+      >
+        {options.map(({ value, label }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+      {helperText && helperText?.length > 0 && (
+        <p
+          id="helper-text-explanation"
+          className="mt-2 text-sm text-gray-500 dark:text-gray-400"
+        >
+          {helperText}
+        </p>
+      )}
+    </div>
+  );
+};
 
-export default SelectBox;
+export { SelectBox };
+export type { SelectOption };
