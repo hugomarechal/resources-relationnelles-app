1559446ecdf2533a9499e7d20e718e3f6d327796
ValHDL <valerie.houdaille@hinodev.com>
2025-04-22 19:25:44 +0200

    Configuration appels API Axios

--- src/App.tsx
+++ src/App.tsx
@@ -47,7 +47,7 @@ function App() {
   //Modal
   const [visible, setVisible] = useState(false);
   const closeModal = () => setVisible(false);
-   
+
   return (
     <>
       <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
---
+++ src/api/apiConfiguration.ts
@@ -0,0 +1,39 @@
+// src/api/api.ts
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://localhost:8080/resources-relationnelles/public/api/',
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+    },
+    timeout: 60000,
+    withCredentials: false,
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('access_token');
+    config.headers = config.headers || {};
+    if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      try {
+        const { response } = error;
+        if (response?.status === 401) {
+          localStorage.removeItem("access_token");
+          // window.location.href = '/login'; // Rediriger vers login si besoin
+        }
+      } catch (e) {
+        console.error('Erreur de gestion 401', e);
+      }
+      return Promise.reject(error);
+    }
+  );
+  
+export default api;
