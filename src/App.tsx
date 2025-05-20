563dc483be8471854342c65a21f1dda87145cb40
Hugo <hugo_m7@live.fr>
2025-05-20 13:47:50 +0200

    Draft home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css";
 import AuthPage from "./Components/Utilisateur/AuthPage";
+import FeedContainer from "./Components/LayoutItems/FeedContainer.tsx";
+import ManageRessources from "./Components/Ressource/ManageRessources.tsx";
 
 
 
@@ -7,15 +9,14 @@ function App() {
 
 return (
     <>
+      {/*replace w/ header*/}
       <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
         <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
           (RE)SOURCES RELATIONNELLES
         </span>
-        
       </h1>
-     
-    <AuthPage />
-    
+        <FeedContainer/>
+    {/*    <ManageRessources/>*/}
     </>
   );
 }
