c33bbe174d17f6bad799506bbb8130dd6f140dc1
vincouille <condellovince2004@gmail.com>
2025-05-20 10:07:39 +0200

    ajout du logo, header, footer, et navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
+import Footer from "./Components/LayoutItems/Footer";
+import Header from "./Components/LayoutItems/Header";
+import Navbar from "./Components/LayoutItems/Navbar";
 import AuthPage from "./Components/Utilisateur/AuthPage";
-
-
 
 function App() {
 
 return (
     <>
+    <Header/>
       <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
         <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
           (RE)SOURCES RELATIONNELLES
@@ -15,7 +17,8 @@ return (
       </h1>
      
     <AuthPage />
-    
+    <Navbar/>
+    <Footer/>
     </>
   );
 }
