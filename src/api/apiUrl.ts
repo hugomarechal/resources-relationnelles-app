c1d11664174612e5ec3b6443795050d42ac8cc2f
ValHDL <valerie.houdaille@hinodev.com>
2025-05-21 14:43:44 +0200

    user

---
+++ src/api/apiUrl.ts
@@ -0,0 +1,11 @@
+// Val
+export const API_BASE_URL = 'http://localhost:8080/resources-relationnelles/public/api/';
+
+// Vince
+// export const API_BASE_URL = 'http://localhost/resources-relationnelles/public/api/';
+
+// Damien
+// export const API_BASE_URL = 'http://127.0.0.1:8000/api/';
+
+// Hugo
+// export const API_BASE_URL = 'http://localhost:8000/api/'; 
