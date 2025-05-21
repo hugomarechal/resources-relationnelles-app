066daf1bc23d91e995e6fee9322d19b944a1b26b
Hugo <132276877+hugomarechal@users.noreply.github.com>
2025-05-21 14:40:00 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import {useUser} from "./contexts/AuthContext.tsx";
 import AdminUserManager from "./Components/Utilisateur/AdminUserManager";
 import Login from "./Components/Utilisateur/Login.tsx";
 import LogoutButton from "./Components/Utilisateur/LogoutButton";
-import AuthPage from "./Components/Utilisateur/AuthPage.tsx";
 import AdminRegister from "./Components/Utilisateur/AdminRegister.tsx";
 
 function App() {
