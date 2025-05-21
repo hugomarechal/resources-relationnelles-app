f8f16d6cdedf3d2a010ae6ea464156d25b819ee9
ValHDL <valerie.houdaille@hinodev.com>
2025-05-21 17:21:08 +0200

    création de compte admon

--- src/Components/Utilisateur/AdminRegister.tsx
+++ src/Components/Utilisateur/AdminRegister.tsx
@@ -1,86 +1,173 @@
-import React, { useState } from 'react';
-import FloatingInput from '../Form/FloatingInput';
-import { SelectBox } from '../Form/SelectBox';
-import { ISelectBoxOption } from '../../types/SelectBoxOption';
-import api from '../../api/apiConfiguration';
-const token = localStorage.getItem('token');
+import React, { useEffect, useState } from "react";
+import FloatingInput from "../Form/FloatingInput";
+import { SelectBox } from "../Form/SelectBox";
+import { ISelectBoxOption } from "../../types/SelectBoxOption";
+import api from "../../api/apiConfiguration";
+import { IRole } from "../../types/Role";
+import { get } from "../../api/apiClient";
+import { ApiResponse } from "../../api/ApiResponse";
+import Button from "../Divers/Button";
+import { FaSave } from "react-icons/fa";
+import { MdClose } from "react-icons/md";
+const token = localStorage.getItem("token");
 
-const AdminRegister: React.FC = () => {
+interface AdminRegisterProps {
+  onClose: () => void;
+}
+
+const AdminRegister: React.FC<AdminRegisterProps> = ({ onClose }) => {
   const [formData, setFormData] = useState({
-    nom: '',
-    prenom: '',
-    pseudo: '',
-    email: '',
-    password: '',
-    password_confirmation: '',
-    code_postal: '',
-    ville: '',
-    role_id: '4' // par défaut citoyen
+    nom: "",
+    prenom: "",
+    pseudo: "",
+    email: "",
+    password: "",
+    password_confirmation: "",
+    code_postal: "",
+    ville: "",
+    role_id: "2", // par défaut administrateur
   });
 
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState("");
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setMessage('');
-    setError('');
+  const handleSubmit = async () => {
+    setMessage("");
+    setError("");
 
     const payload = {
       ...formData,
-      actif: true
+      actif: true,
     };
 
     try {
-      const res = await api.post('users', payload, {
+      await api.post("users", payload, {
         headers: {
-        Authorization: `Bearer ${token}`
-        }});
-      setMessage('Compte créé avec succès !');
-      setFormData({
-        nom: '',
-        prenom: '',
-        pseudo: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
-        code_postal: '',
-        ville: '',
-        role_id: '4'
+          Authorization: `Bearer ${token}`,
+        },
       });
+      setMessage("Compte créé avec succès !");
+      setFormData({
+        nom: "",
+        prenom: "",
+        pseudo: "",
+        email: "",
+        password: "",
+        password_confirmation: "",
+        code_postal: "",
+        ville: "",
+        role_id: "4",
+      });
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
-      setError(err.response?.data?.message || 'Erreur lors de la création du compte.');
+      setError(
+        err.response?.data?.message || "Erreur lors de la création du compte."
+      );
     }
   };
 
   const villesOptions: ISelectBoxOption[] = [
-    { label: 'Paris', value: 'Paris' },
-    { label: 'Lyon', value: 'Lyon' },
-    { label: 'Marseille', value: 'Marseille' }
+    { label: "Paris", value: "Paris" },
+    { label: "Lyon", value: "Lyon" },
+    { label: "Marseille", value: "Marseille" },
+    { label: "Toulouse", value: "Toulouse" },
+    { label: "Nice", value: "Nice" },
+    { label: "Nantes", value: "Nantes" },
+    { label: "Strasbourg", value: "Strasbourg" },
+    { label: "Montpellier", value: "Montpellier" },
+    { label: "Bordeaux", value: "Bordeaux" },
+    { label: "Lille", value: "Lille" },
   ];
 
+  // //Récupération de toutes les catégories
+  const [roles, setRoles] = useState<IRole[]>([]);
+
+  const getAllCategories = async () => {
+    const response = await get<ApiResponse<IRole[]>>("roles");
+    if (response?.status && response.data) {
+      setRoles(response.data);
+    }
+  };
+
+  useEffect(() => {
+    getAllCategories();
+  }, []);
+
   const rolesOptions: ISelectBoxOption[] = [
-    { label: 'Super administrateur', value: '1' },
-    { label: 'Administrateur', value: '2' },
-    { label: 'Modérateur', value: '3' },
-    { label: 'Citoyen', value: '4' }
+    ...roles.map((role) => ({
+      label: role.name,
+      value: String(role.id),
+    })),
   ];
 
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-gray-10 rounded shadow">
-      <h2 className="text-2xl font-bold mb-4 text-center text-gray-10">Créer un compte administrateur</h2>
-      <form onSubmit={handleSubmit} className="space-y-6">
-        <FloatingInput type="text" label="Nom" name="nom" value={formData.nom} onChange={handleChange} required />
-        <FloatingInput type="text" label="Prénom" name="prenom" value={formData.prenom} onChange={handleChange} required />
-        <FloatingInput type="text" label="Pseudo" name="pseudo" value={formData.pseudo} onChange={handleChange} required />
-        <FloatingInput type="email" label="Email" name="email" value={formData.email} onChange={handleChange} required />
-        <FloatingInput type="password" label="Mot de passe" name="password" value={formData.password} onChange={handleChange} required />
-        <FloatingInput type="password" label="Confirmation du mot de passe" name="password_confirmation" value={formData.password_confirmation} onChange={handleChange} required />
-        <FloatingInput type="text" label="Code postal" name="code_postal" value={formData.code_postal} onChange={handleChange} required />
+      <h2 className="text-2xl font-bold mb-4 text-center text-gray-10">
+        Créer un compte d'administration
+      </h2>
+      <form className="space-y-6">
+        <FloatingInput
+          type="text"
+          label="Nom"
+          name="nom"
+          value={formData.nom}
+          onChange={handleChange}
+          required
+        />
+        <FloatingInput
+          type="text"
+          label="Prénom"
+          name="prenom"
+          value={formData.prenom}
+          onChange={handleChange}
+          required
+        />
+        <FloatingInput
+          type="text"
+          label="Pseudo"
+          name="pseudo"
+          value={formData.pseudo}
+          onChange={handleChange}
+          required
+        />
+        <FloatingInput
+          type="email"
+          label="Email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        <FloatingInput
+          type="password"
+          label="Mot de passe"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <FloatingInput
+          type="password"
+          label="Confirmation du mot de passe"
+          name="password_confirmation"
+          value={formData.password_confirmation}
+          onChange={handleChange}
+          required
+        />
+        <FloatingInput
+          type="text"
+          label="Code postal"
+          name="code_postal"
+          value={formData.code_postal}
+          onChange={handleChange}
+          required
+        />
 
         <p className="text-sm text-gray-400 mb-1 text-left">Ville</p>
         <SelectBox
@@ -105,9 +192,18 @@ const AdminRegister: React.FC = () => {
         {error && <p className="text-red-500 text-sm">{error}</p>}
         {message && <p className="text-green-600 text-sm">{message}</p>}
 
-        <button type="submit" className="w-full bg-clip bg-gradient-to-r to-emerald-600 from-sky-400 text-white py-2 rounded hover:bg-blue-700">
-          Créer un compte
-        </button>
+        <div className="flex justify-center gap-4">
+          <Button
+            label="Enregistrer"
+            icon={<FaSave className="w-4 h-4 mr-2" />}
+            onClick={handleSubmit}
+          />
+          <Button
+            label="Fermer"
+            icon={<MdClose size={25} />}
+            onClick={onClose}
+          />
+        </div>
       </form>
     </div>
   );
--- src/Components/Utilisateur/AdminUserManager.tsx
+++ src/Components/Utilisateur/AdminUserManager.tsx
@@ -1,9 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import SwitchToggle from '../Divers/SwitchToggle';
-import { FaPlus } from 'react-icons/fa';
-import Button from '../Divers/Button';
-import Modal from '../Divers/Modal'; 
-import AdminRegister from '../Utilisateur/AdminRegister'; 
+import React, { useEffect, useState } from "react";
+import SwitchToggle from "../Divers/SwitchToggle";
+import { FaPlus } from "react-icons/fa";
+import Button from "../Divers/Button";
+import Modal from "../Divers/Modal";
+import AdminRegister from "../Utilisateur/AdminRegister";
+import { API_BASE_URL } from "../../api/apiUrl";
+import { MdClose } from "react-icons/md";
 
 interface User {
   id: number;
@@ -18,34 +20,34 @@ const AdminUserManager: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [modalFormVisible, setModalFormVisible] = useState(false);
 
- const fetchUsers = async () => {
-  try {
-    setLoading(true);
-    const res = await fetch('http://127.0.0.1:8000/api/users', {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem('token')}`
-      }
-    });
-    const result = await res.json();
-    setUsers(result.data); 
-  } catch (err) {
-    console.error('Erreur lors de la récupération des utilisateurs.');
-  } finally {
-    setLoading(false);
-  }
-};
+  const fetchUsers = async () => {
+    try {
+      setLoading(true);
+      const res = await fetch(API_BASE_URL + "users", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      const result = await res.json();
+      setUsers(result.data);
+    } catch {
+      console.error("Erreur lors de la récupération des utilisateurs.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const toggleUser = async (id: number) => {
     try {
-      await fetch(`http://127.0.0.1:8000/api/admin/users/${id}/toggle`, {
-        method: 'PUT',
+      await fetch(API_BASE_URL + `admin/users/${id}/toggle`, {
+        method: "PUT",
         headers: {
-          Authorization: `Bearer ${localStorage.getItem('token')}`
-        }
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
       });
       fetchUsers(); // recharge la liste après changement
-    } catch (err) {
-      console.error('Erreur lors de l’activation/désactivation.');
+    } catch {
+      console.error("Erreur lors de l’activation/désactivation.");
     }
   };
 
@@ -94,11 +96,11 @@ const AdminUserManager: React.FC = () => {
               <td className="px-6 py-2">{user.prenom}</td>
               <td className="px-6 py-2">{user.email}</td>
               <td className="px-2 py-2">
-                { 
-                <SwitchToggle
-                  checked={user.actif}
-                  onToggle={() => toggleUser(user.id)}
-                />
+                {
+                  <SwitchToggle
+                    checked={user.actif}
+                    onToggle={() => toggleUser(user.id)}
+                  />
                 }
               </td>
             </tr>
@@ -106,26 +108,17 @@ const AdminUserManager: React.FC = () => {
         </tbody>
       </table>
       {modalFormVisible && (
-  <Modal
-    isOpen={modalFormVisible}
-    onClose={() => setModalFormVisible(false)}
-    dismissable={true}
-    position="center"
-  >
-    <div className="text-black p-4">
-      <h3 className="text-xl font-semibold">Création d’un compte</h3>
-      <AdminRegister />
-
-      <button
-        className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
-        onClick={() => setModalFormVisible(false)}
-      >
-        Fermer
-      </button>
-    </div>
-  </Modal>
-)}
-
+        <Modal
+          isOpen={modalFormVisible}
+          onClose={() => setModalFormVisible(false)}
+          dismissable={true}
+          position="center"
+        >
+          <div className="text-black p-4">
+            <AdminRegister onClose={() => setModalFormVisible(false)} />
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
